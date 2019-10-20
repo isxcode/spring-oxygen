5cfeee4028a0c684221c72fd5b59524e83b9a982
ispong <ispong@outlook.com>
2019-10-20 22:34:26 +0800

    [init] 项目初始化

---
+++ src/main/java/com/isxcode/isxcodespring/IsxcodeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.isxcode.isxcodespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsxcodeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsxcodeSpringApplication.class, args);
+	}
+
+}
