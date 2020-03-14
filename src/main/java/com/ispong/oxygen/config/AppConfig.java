c3bc3827f5ef33300b0738660bb46578819632ce
ispong <ispong@outlook.com>
2020-03-14 16:30:25 +0800

    [update] 修改readme

--- src/main/java/com/ispong/oxygen/config/AppConfig.java
+++ src/main/java/com/ispong/oxygen/config/AppConfig.java
@@ -1,43 +1,37 @@
-package com.ispong.oxygen.config;
-
-import com.ispong.oxygen.flysql.annotation.EnableFlysql;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-
-/**
- * 项目基础配置
- *
- * @author ispong
- * @version v0.1.0
- */
-@EnableFlysql
-@EnableConfigurationProperties
-@EnableTransactionManagement
-@Configuration
-@EnableAspectJAutoProxy
-@EnableScheduling
-@EnableAsync
-@ImportResource(locations = {"classpath:schema/**"})
-public class AppConfig {
-
-	/**
-	 * 打印ApplicationContext
-	 *
-	 * @param appContext ApplicationContext
-	 * @since 2019/10/17
-	 */
-//	@Bean
-//	public CommandLineRunner run(ApplicationContext appContext) {
-//		return args -> Arrays.stream(appContext.getBeanDefinitionNames()).sorted().forEach(System.out::println);
-//	}
-
-}
-
-
+//package com.ispong.oxygen.config;
+//
+////import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//
+//import com.ispong.oxygen.flysql.annotation.EnableFlysql;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * 项目基础配置
+// *
+// * @author ispong
+// * @version v0.1.0
+// */
+//@EnableFlysql
+//@Configuration
+////@EnableConfigurationProperties
+////@EnableTransactionManagement
+////@EnableAspectJAutoProxy
+////@EnableScheduling
+////@EnableAsync
+////@ImportResource(locations = {"classpath:schema/**"})
+//public class AppConfig {
+//
+//	/**
+//	 * 打印ApplicationContext
+//	 *
+//	 * @param appContext ApplicationContext
+//	 * @since 2019/10/17
+//	 */
+////	@Bean
+////	public CommandLineRunner run(ApplicationContext appContext) {
+////		return args -> Arrays.stream(appContext.getBeanDefinitionNames()).sorted().forEach(System.out::println);
+////	}
+//
+//}
+//
+//
