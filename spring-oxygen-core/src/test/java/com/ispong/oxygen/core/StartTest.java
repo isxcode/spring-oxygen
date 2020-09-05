4265ba243e46d98931389f1b7e43ac3ddab98534
ispong <ispong@outlook.com>
2020-09-05 16:57:22 +0800

    :sparkles: update to analysis code

---
+++ spring-oxygen-core/src/test/java/com/ispong/oxygen/core/StartTest.java
@@ -0,0 +1,11 @@
+package com.ispong.oxygen.core;
+
+import org.junit.jupiter.api.Test;
+
+public class StartTest {
+
+    @Test
+    public void HelloTest(){
+        System.out.println("hello spring-oxygen");
+    }
+}
