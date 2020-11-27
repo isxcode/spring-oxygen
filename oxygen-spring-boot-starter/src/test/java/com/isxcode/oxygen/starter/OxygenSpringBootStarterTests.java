46846baac063c3a22b7341f306d65111f4c23995
ispong <ispong@outlook.com>
2020-11-27 09:38:10 +0800

    :feature:  update starter (#70)
    * :fire: delete starter license
    
    * :tada: add package-info.java
    
    * :sparkles: update spring.factories add EnvironmentPostProcessor

--- oxygen-spring-boot-starter/src/test/java/com/isxcode/oxygen/starter/OxygenSpringBootStarterTests.java
+++ oxygen-spring-boot-starter/src/test/java/com/isxcode/oxygen/starter/OxygenSpringBootStarterTests.java
@@ -5,8 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-@ContextConfiguration(classes = OxygenAutoConfiguration.class)
-public class SpringOxygenStarterTests {
+@ContextConfiguration(classes = OxygenStarterAutoConfiguration.class)
+public class OxygenSpringBootStarterTests {
 
     @Test
     public void test() {
