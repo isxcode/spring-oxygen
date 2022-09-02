56fb16fa7f1c8038d4703cbe3cccbb281bd0ee2e
ispong <ispong@outlook.com>
2022-09-02 11:57:09 +0800

    :sparkles: support set userId to OxygenHolder #291

--- spring-oxygen-flysql/src/main/java/com/isxcode/oxygen/flysql/common/OxygenHolder.java
+++ spring-oxygen-flysql/src/main/java/com/isxcode/oxygen/flysql/common/OxygenHolder.java
@@ -1,6 +1,10 @@
 package com.isxcode.oxygen.flysql.common;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Collection;
 
 public class OxygenHolder {
 
@@ -15,4 +19,62 @@ public class OxygenHolder {
 
         return String.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
     }
+
+    public static void setUserUuid(String userUuid) {
+
+        Authentication authentication = new Authentication() {
+            @Override
+            public boolean equals(Object another) {
+                return false;
+            }
+
+            @Override
+            public String toString() {
+                return null;
+            }
+
+            @Override
+            public int hashCode() {
+                return 0;
+            }
+
+            @Override
+            public String getName() {
+                return null;
+            }
+
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return null;
+            }
+
+            @Override
+            public Object getCredentials() {
+                return null;
+            }
+
+            @Override
+            public Object getDetails() {
+                return null;
+            }
+
+            @Override
+            public Object getPrincipal() {
+                return userUuid;
+            }
+
+            @Override
+            public boolean isAuthenticated() {
+                return false;
+            }
+
+            @Override
+            public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+            }
+        };
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
 }
