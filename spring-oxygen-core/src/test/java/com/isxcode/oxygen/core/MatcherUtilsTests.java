f1a0eb19bf0a9273954350e1bcb9d606b62700c2
ispong <ispong@outlook.com>
2022-09-06 10:12:56 +0800

    :white_check_mark: add matcherUtils #297

---
+++ spring-oxygen-core/src/test/java/com/isxcode/oxygen/core/MatcherUtilsTests.java
@@ -0,0 +1,16 @@
+package com.isxcode.oxygen.core;
+
+import com.isxcode.oxygen.core.matcher.MatcherUtils;
+import org.junit.jupiter.api.Test;
+
+public class MatcherUtilsTests {
+
+    @Test
+    public void testExcelParseFile() {
+
+        String url = "https://baidu.com";
+        String matcherParse = MatcherUtils.matcherParse("https://", ".com", url);
+        System.out.println(matcherParse);
+    }
+
+}
