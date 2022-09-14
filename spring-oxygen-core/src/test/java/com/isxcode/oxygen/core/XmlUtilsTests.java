54c956675344269c4195df1603dc66b4ea3f9c66
ispong <ispong@outlook.com>
2022-09-14 11:22:27 +0800

    :bug: ExcelUtils not support LOCAL_DATE #300

--- spring-oxygen-core/src/test/java/com/isxcode/oxygen/core/XmlUtilsTests.java
+++ spring-oxygen-core/src/test/java/com/isxcode/oxygen/core/XmlUtilsTests.java
@@ -4,15 +4,43 @@ import com.isxcode.oxygen.core.pojo.Dog;
 import com.isxcode.oxygen.core.xml.XmlUtils;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class XmlUtilsTests {
 
     @Test
     public void testXml() {
 
         String xmlStr = "<xml><name>wang</name><age>12</age></xml>";
+        Path path = Paths.get("Dogs.xml");
+        try {
+            Files.write(path, xmlStr.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         Dog dog = XmlUtils.parseXmlString(xmlStr, Dog.class);
-        System.out.println("dog:" + dog.toString());
+        System.out.println("dog:" + dog);
     }
 
+    @Test
+    public void testXmlFile() {
+
+        Dog dog;
+        Path path = Paths.get("Dogs.xml");
+        try {
+            dog = XmlUtils.parseXmlInputStream(Files.newInputStream(path), Dog.class);
+            System.out.println("dog:" + dog);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
 
