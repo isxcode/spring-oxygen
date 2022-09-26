7f7058627d9a348b797dcdf6d7dfd928d18aaca9
ispong <ispong@outlook.com>
2022-09-26 15:33:51 +0800

    :art: art freemarker utils #308

--- spring-oxygen-core/src/test/java/com/isxcode/oxygen/core/HttpUtilsTests.java
+++ spring-oxygen-core/src/test/java/com/isxcode/oxygen/core/HttpUtilsTests.java
@@ -11,40 +11,40 @@ import java.util.Map;
 public class HttpUtilsTests {
 
     @Test
-    public void testPostHttp() {
+    public void testPostHttp() throws IOException {
 
-        String url = "http://portal.definesys.com:30502/xdap-admin/msg/count/newMessage?timestamp=1607569924902";
+        String url = "https://api.isxcode.com/leo/version";
 
         Map<String, String> headers = new HashMap<>();
-        headers.put("xdaptenantid", "76714728949415937");
-        headers.put("xdaptenantversion", "77404393242624000");
-        headers.put("xdaptimestamp", "1607569924902");
-        headers.put("xdaptoken", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2MDc1NzIxODUsImlhdCI6MTYwNzU2NDk4NSwieGRhcHVzZXJpZCI6IjEwMDEwNDYxNDczMjI0MDI1NzAyNCJ9.MkEVO4aM2DGmDeoZhoAm-SIcWl0zxww9FHIrl0UZJ6_816jSZQQhDfcJgU58nKwH_juj9SNSHXaih1W86WNiXg");
-        headers.put("xdapversion", "1.0.3");
+        headers.put("user", "ispong");
+
+        Map<String, String> requestParams = new HashMap<>();
+        requestParams.put("user", "ispong");
 
         try {
-            System.out.println(HttpUtils.doPost(url, headers, "", String.class));
+            System.out.println(HttpUtils.doPost(url, headers, requestParams, String.class));
         } catch (OxygenException | IOException e) {
             System.out.println(e.getMessage());
+            throw e;
         }
     }
 
     @Test
     public void testGetHttp() {
 
-        String url = "http://portal.definesys.com:30502/xdap-app/form/query/formConfigAndStatus?timestamp=1607570430133&formId=5fd03687543d7b3e8f107ac9&documentId=124457730072121344&type=DETAIL_FORM";
+        String url = "https://api.isxcode.com/leo/version";
 
         Map<String, String> headers = new HashMap<>();
-        headers.put("xdaptenantid", "76714728949415937");
-        headers.put("xdaptenantversion", "77404394242624000");
-        headers.put("xdaptimestamp", "1607570344954");
-        headers.put("xdaptoken", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2MDc1NzIxODUsImlhdCI6MTYwNzU2NDk4NSwieGRhcHVzZXJpZCI6IjEwMDEwNDYxNDczMjI0MDI1NzAyNCJ9.MkEVO4aM2DGmDeoZhoAm-SIcWl0zxww9FHIrl0UZJ6_816jSZQQhDfcJgU58nKwH_juj9SNSHXaih1W86WNiXg");
-        headers.put("xdapversion", "1.0.3");
+        headers.put("user", "ispong");
+
+        Map<String, String> requestParams = new HashMap<>();
+        requestParams.put("user", "ispong");
 
         try {
-            System.out.println(HttpUtils.doGet(url, headers, String.class));
+            System.out.println(HttpUtils.doGet(url, headers, requestParams, String.class));
         } catch (OxygenException e) {
             System.out.println(e.getMessage());
+            throw e;
         }
     }
 }
