74c254196e43c8e692e0c8a1efd9049abb1cdb86
ispong <ispong@outlook.com>
2022-10-11 10:14:03 +0800

    :bug: fix commandUtils logfile always in process

--- spring-oxygen-core/src/test/java/com/isxcode/oxygen/core/CommandUtilsTests.java
+++ spring-oxygen-core/src/test/java/com/isxcode/oxygen/core/CommandUtilsTests.java
@@ -1,17 +1,22 @@
 package com.isxcode.oxygen.core;
 
 import com.isxcode.oxygen.core.command.CommandUtils;
+import com.isxcode.oxygen.core.file.FileUtils;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
 public class CommandUtilsTests {
 
-	@Test
+    @Test
+	@SneakyThrows
 	public void testCommand() {
 
 		System.out.println(CommandUtils.executeBack("dir"));
 
-		System.out.println(CommandUtils.execute("dir", "command.log"));
-
 		System.out.println(CommandUtils.execute("dir"));
-	}
+
+        System.out.println(CommandUtils.execute("dir", "command.log"));
+
+        FileUtils.recursionDeleteFile("command.log");
+    }
 }
